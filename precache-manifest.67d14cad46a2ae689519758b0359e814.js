413e4ae30ac6907747f1f0b2ce5b8b353cd450d7
snow <hsuehweh@usc.edu>
2020-05-17 13:26:51 -0700

    Updates

---
+++ precache-manifest.67d14cad46a2ae689519758b0359e814.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "093718858b0aff602738e339e04532b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f368fc7a4f57d3269216",
+    "url": "/static/css/main.b467da2a.chunk.css"
+  },
+  {
+    "revision": "4ed9fd4c842519f24ef4",
+    "url": "/static/js/2.233764bf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.233764bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f368fc7a4f57d3269216",
+    "url": "/static/js/main.9c92a2ec.chunk.js"
+  },
+  {
+    "revision": "56de2ffbb97df600f39e",
+    "url": "/static/js/runtime-main.ebeab046.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CheckerGame/precache-manifest.dd692e0cfb46c87a7c4ab14d6c5d856c.js"
+  "/precache-manifest.67d14cad46a2ae689519758b0359e814.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CheckerGame/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
